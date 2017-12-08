611c23b408436e766714fef11e1a6de3fa957ada
Kelsey Langlois <kels.langlois@gmail.com>
2017-12-08 14:19:03 -0800

    Add test for adding item to cart with AJAX.

--- spec/features/product_ajax_spec.rb
+++ spec/features/product_ajax_spec.rb
@@ -1,11 +1,19 @@
 require "rails_helper"
 
-describe "the product path with AJAX" do
-  it "uses AJAX to display product details in a modal", js: true do
+describe "the product path with AJAX", js: true do
+  it "uses AJAX to display product details in a modal" do
     product = FactoryBot.create(:product)
     visit products_path
     click_on product.name
     expect(page).to have_content(product.description)
     expect(page).to have_content('CLOSE')
+  end
+
+  it "uses AJAX to add an item to a user's cart" do
+    product = FactoryBot.create(:product)
+    visit products_path
+    click_on product.name
+    click_button "add_to_cart"
+    expect(page).to have_content("Cart | Total Items: 1")
   end
 end
