4b03582b0f1ddffc28aef395c011df92e99f9d10
Kelsey Langlois <kels.langlois@gmail.com>
2017-12-08 09:30:38 -0800

    Update db and seed file to include Paperclip images.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,14 @@ class Seed
   end
 
   def generate_products
+    Product.destroy_all
     20.times do |i|
-      Product.create!(name: Faker::Lorem.word, price: rand(10...100), description: Faker::Lorem.sentence(5, false, 0).chop)
+      Product.create!(
+        name: Faker::Lorem.word,
+        price: rand(10...100),
+        description: Faker::Lorem.sentence(5, false, 0).chop,
+        image: File.new("#{Rails.root}/public/images/medium/sample.jpeg")
+      )
     end
   end
 end
