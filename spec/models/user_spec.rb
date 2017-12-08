7f9a40ac80155b2ca6e03753194707452846709e
Kelsey Langlois <kels.langlois@gmail.com>
2017-12-08 13:40:23 -0800

    Add tests for user model methods.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,17 @@ describe User do
   it { should validate_presence_of :password_confirmation }
   it { should validate_uniqueness_of :name }
   it { should validate_uniqueness_of :email }
+
+  describe '#previous_orders' do
+    it "returns all orders assigned to a user with status 2" do
+      user = FactoryBot.create(:user, name: "New Test", email: "new@new.com")
+      order1 = FactoryBot.create(:order, user: user, status: 2)
+      user.orders.push(order1)
+      order2 = FactoryBot.create(:order, user: user)
+      user.orders.push(order2)
+      order3 = FactoryBot.create(:order, user: user, status: 2)
+      user.orders.push(order3)
+      expect(user.previous_orders).to eq([order1, order3])
+    end
+  end
 end
