34b82e5ec19104877f63680772d5e5ac960b4364
Kelsey Langlois <kels.langlois@gmail.com>
2017-12-08 12:01:06 -0800

    Add full CRUD for products.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,10 +17,34 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
+      flash[:notice] = "Product created!"
       redirect_to '/'
     else
+      flash[:alert] = @product.errors.full_messages
       render :new
     end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      flash[:notice] = "Product updated!"
+      redirect_to '/'
+    else
+      flash[:alert] = @product.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    flash[:notice] = "Product deleted."
+    redirect_to '/'
   end
 
 private
