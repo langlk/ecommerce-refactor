bc0da6f242169753daeb9cf3be17bf221bc07055
Kelsey Langlois <kels.langlois@gmail.com>
2017-12-08 14:30:14 -0800

    Add test for removing item from cart with AJAX.

--- spec/features/product_ajax_spec.rb
+++ spec/features/product_ajax_spec.rb
@@ -16,4 +16,14 @@ describe "the product path with AJAX", js: true do
     click_button "add_to_cart"
     expect(page).to have_content("Cart | Total Items: 1")
   end
+
+  it "uses AJAX to remove an item from a user's cart" do
+    product = FactoryBot.create(:product)
+    visit product_path(product)
+    click_button "add_to_cart"
+    click_link "Cart | Total Items: 1"
+    click_link "Remove"
+    expect(page).to have_content("Cart | Total Items: 0")
+    expect(page).to have_no_content(product.name)
+  end
 end
