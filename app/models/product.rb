250adee7992725510027a300e760c2a735e3df7c
Kelsey Langlois <kels.langlois@gmail.com>
2017-12-08 11:34:02 -0800

    Add product validations for presence and numericality, and test.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
   has_many :order_items
-  
+  validates :name, :description, :price, presence: true
+  validates :price, numericality: { greater_than: 0.0 }
+
   # Paperclip
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.jpeg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
