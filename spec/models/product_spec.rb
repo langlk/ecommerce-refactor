b61db01f76a92377cb7e2a5db3b82cc519d14340
Kelsey Langlois <kels.langlois@gmail.com>
2017-12-11 08:12:24 -0800

    Update Product to remove lnked items before delete.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,4 +7,13 @@ describe Product do
   it { should validate_presence_of :description }
   it { should validate_presence_of :price }
   it { should validate_numericality_of(:price).is_greater_than(0.0) }
+
+  describe '#remove_linked_items' do
+    it "removes any linked order items before destroying a product" do
+      product = FactoryBot.create(:product)
+      item = FactoryBot.create(:order_item, product: product)
+      product.destroy
+      expect(OrderItem.all).to eq([])
+    end
+  end
 end
